784e8c57d454e977943d6115d611e10553402153
UGYYIH <191670835@qq.com>
2023-10-26 22:02:32 +0800

    JL

---
+++ Informer2020-main/results/informer_WTH_ftM_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_0/dtf.py
@@ -0,0 +1,25 @@
+import numpy as np
+data = np.load("pred.npy")
+print(data)
+
+# import numpy as np
+# data = np.load("metrics.npy")
+# print(data)
+
+# import numpy as np
+# data = np.load("true.npy")
+# print(data)
+
+# import numpy as np
+
+
+
+
+# # 设置 NumPy 打印选项
+# np.set_printoptions(threshold=np.inf)
+#
+# # 加载并打印数组
+# data = np.load('pred.npy')
+# print(data)
+
+
